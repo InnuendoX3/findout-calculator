68056984f2cb1b350288562215678688225c0d52
Johan Garzon <johanprograma@hotmail.com>
2020-01-14 23:43:40 +0100

    Program ends when writting: quit

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ var readline = require('readline');
 var rl = readline.createInterface({
    input: process.stdin,
    output: process.stdout,
-   prompt: '> '
+   //prompt: '> '
  });
 
-rl.question('Como es tu nombre? ', (respuesta) => {
+/* rl.question('Como es tu nombre? ', (respuesta) => {
    console.log(`Hola ${respuesta}`);
    rl.prompt();
-});
+}); */
+
+rl.on('line', (input) => {
+   input = input.trim().toLowerCase();
+   if (input == "quit") {
+      process.exit();   
+   }
+   console.log(input);
+})
