70566ad5168025799736a468c609bcf858729487
Johan Garzon <johanprograma@hotmail.com>
2020-01-15 10:47:33 +0100

    Identifying the users input with switch/case

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-var readline = require('readline');
+const rl = require("./prog-modules/config/readline");
 
-var rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout,
-   //prompt: '> '
- });
+//let operations = 
 
-/* rl.question('Como es tu nombre? ', (respuesta) => {
-   console.log(`Hola ${respuesta}`);
-   rl.prompt();
-}); */
-
-rl.on('line', (input) => {
+rl.on("line", (input) => {
    input = input.trim().toLowerCase();
    if (input == "quit") {
-      process.exit();   
+      process.exit();
    }
-   console.log(input);
-})
+
+   identifyInput(input);
+});
+
+/*  
+   Identify if user wants to:
+   - Make an register (3 parts)
+   - Print (2 parts)
+   - Exit (1 part)
+   Return 'order' {
+      type: register, print or exit
+      parts: array with the parts
+      error: message on error
+   }
+ */
+function identifyInput(input) {
+   let parts = input.split(" ");
+   let partsQty = parts.length;
+   let order = {};
+   switch (partsQty) {
+      case 0:
+         order.error ="Enter a command. Trying to end the program? Enter: Quit";
+      case 1:
+         order.type = "exit";
+         break;
+      case 2:
+         order.type = "print";
+         break;
+      case 3:
+         order.type = "register";
+         break;
+      default:
+         order.error = "If you are trying to register follow this systax: <register> <operation> <value>";
+         break;
+   }
+
+   console.log(order)
+   console.log(parts)
+
+/*    for (const part of parts) {
+      console.log(part);
+   } */
+};
---
+++ prog-modules/config/readline.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+
+// 'readline' configuration
+const rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout,
+ });
+
+ module.exports = rl;
